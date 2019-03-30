74cdf0d3983168647c55aeae0b39bf84c556ac8b
freediskless <48971078+freediskless@users.noreply.github.com>
2019-03-31 01:26:38 +0700

    Create licenseauth.php

---
+++ licenseauth.php
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Result
+error="0"
+license_name="bdstd"
+license_password="bdstd"
+license_expired="2999-12-31"
+license_ccboot=""
+/>
+<urls>
+<homepageurl value="" />
+<helpurl value="" />
+<userforumurl value="" />
+<facebookurl value="" />
+<managementurl value="http://bit.ly/icafemenu_nothingtodohere?" />
+<bugreportsurl value="" />
+<signupurl value="" />
+<forgetpwdurl value="" />
+</urls>
